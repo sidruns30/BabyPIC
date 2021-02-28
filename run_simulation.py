50c5edf1c44d1c61e6ab609e8a5ac14e97426565
sidruns30 <siddhantsolanki@ucsb.edu>
2021-02-28 12:20:32 -0800

    Added boundary conditions (EM fields only)

--- run_simulation.py
+++ run_simulation.py
@@ -29,6 +29,8 @@ from fpfe_class import FPFE
 from poisson import charge_deposition, grad_v
 from particle_push import push
 from current_deposition import deposit
+from boundary_conditions import set_boundary_condition_fields
+from boundary_conditions import set_boundary_condition_fpfe
 
 import numpy as np
 from numpy.linalg import norm 
@@ -37,20 +39,22 @@ from numpy.linalg import norm
 # Set the grid properties 
 class GRID:
     def __init__(self):
-        self.nx = 300
-        self.ny = 300
-        self.xmin = -2
-        self.xmax = 2
-        self.ymin = -2
-        self.ymax = 2
+        self.nx = 50
+        self.ny = 50
+        self.xmin = -5
+        self.xmax = 5
+        self.ymin = -5
+        self.ymax = 5
 GridParam = GRID()
 
-# Set the FPFE properties 
-N_FPFE = 2
-charges = [-1, 1]
-masses = [1, 1]
-positions = np.array([[-0.1,0], [0.1,0]])
-velocities = np.array([[-0.5,0], [0.5,0]])
+### Set the FPFE properties 
+N_FPFE = 200
+charges = 1e-1*np.ones(N_FPFE)
+masses = 1e4*np.ones(N_FPFE)
+# Set positions
+theta = np.linspace(0, 2*np.pi, N_FPFE)
+positions = np.array([[np.cos(t), np.sin(t)] for t in theta])
+velocities = np.array([[-np.sin(t)/5, np.cos(t)/5] for t in theta])
 FPFE_list = []
 for i in range(N_FPFE):
     FPFE_list.append(FPFE(GridParam, q=charges[i], m=masses[i], 
@@ -67,7 +71,7 @@ STEP 0
 charge_deposition(FieldsObject, FPFE_list)
 grad_v(FieldsObject)
 # Add divergence free background electric field to existing field
-Ex_div_free = -1*np.ones((GridParam.nx+2, GridParam.ny+1))
+Ex_div_free = 0*np.ones((GridParam.nx+2, GridParam.ny+1))
 Ey_div_free = np.zeros((GridParam.nx+1, GridParam.ny+2))
 
 FieldsObject.Ex_new[:,:] += Ex_div_free
@@ -75,6 +79,7 @@ FieldsObject.Ey_new[:,:] += Ey_div_free
 
 ### Define one simulation timestep 
 def timestep(FieldsObject, FPFE_list):
+    step = 0
     # STEPS 1 and 2 - Update particle positions + deposit currents on the grid
     for FpfeObject in FPFE_list:
         FpfeObject.update_pos()
@@ -82,8 +87,11 @@ def timestep(FieldsObject, FPFE_list):
     # STEP 3 - Update Electric Fields 
     FieldsObject.update_ex()
     FieldsObject.update_ey()
+    set_boundary_condition_fields(FieldsObject, step, 'periodic', 'periodic')
     # STEP 4 - Update Magnetic Fields 
+    step = 1/2
     FieldsObject.update_bz()
+    set_boundary_condition_fields(FieldsObject, step, 'periodic', 'periodic')
     # STEP 5 - Update particle velocities 
     for FpfeObject in FPFE_list:
         push(FieldsObject, FpfeObject)
@@ -105,14 +113,17 @@ for i in range(timesteps):
         import matplotlib.pyplot as plt
         X,Y = np.meshgrid(FieldsObject.x_edges, FieldsObject.y_edges)
         for FpfeObject in FPFE_list:
-            plt.plot(FpfeObject.r_new[0], FpfeObject.r_new[1], 'o')
+            plt.plot(FpfeObject.r_new[0], FpfeObject.r_new[1], 'go', alpha=0.1)
         #plt.pcolormesh(X,Y,FieldsObject.rho.T)
         plt.pcolormesh(X[1:-1,1:-1],Y[1:-1,1:-1],FieldsObject.Bz_old[1:-1,1:-1].T, 
-                       cmap='seismic', vmin=-10, vmax=10)
+                       cmap='seismic', vmin=-5, vmax=5)
         plt.colorbar()
         
         Ex = (FieldsObject.Ex_old[1:,:] + FieldsObject.Ex_old[:-1,:])/2
         Ey = (FieldsObject.Ey_old[:,1:] + FieldsObject.Ey_old[:,:-1])/2
         plt.streamplot(X[1:-1,1:-1],Y[1:-1,1:-1],Ex.T,Ey.T, color='k')
+        plt.xlim((GridParam.xmin, GridParam.xmax))
+        plt.ylim((GridParam.ymin, GridParam.ymax))
+        plt.title('$B_z$', fontsize=20)
         plt.savefig('plots/fig'+str(i//20)+'.png')
         plt.close()
